6a77408d8ff5439238be0d10456a0af002cd82bb
Dmitry <d.smirnoff.73@gmail.com>
2019-09-10 12:17:30 +0200

    Проврка на четность/done

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import {getName} from '..';
 
-console.log('Welcome to the Brain Games!');
-console.log(`Hello, ${getName()}!`);
+import {introduction} from '..';
+
+introduction();
--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import readlineSync from 'readline-sync';
 
-export const getName = () => readlineSync.question('May I have your name? :');
+// export const getName = () => readlineSync.question('May I have your name? :');
+const getRandom = (min, max) => Math.floor(Math.random() * (max - min) + min);
+
+export const introduction = (rulesMssg = '') => {
+    console.log('\nWelcome to the Brain Games!\n', rulesMssg);
+    const name = readlineSync.question('\nMay I have your name? : ');
+    console.log(`Hello, ${name}!`);
+    return name;
+};
+
+const tryGuess = (name, numberOfTries) => {
+    if (!numberOfTries) return `\nCongratulations, ${name}!`;
+
+    const randomRange = [0, 100];
+    const guessNumber = getRandom(...randomRange);
+    const isEven = (guessNumber % 2 === 0) ? 'yes' : 'no';
+    const answer = (readlineSync.question(`\nQuestion: ${guessNumber}\nYour answer?: `) === 'yes') ? 'yes' : 'no';
+
+    if (isEven !== answer) {
+        return `\n'${answer}' is a wrong answer ;(. Correct answer was '${isEven}'\nLet's try again sometime, ${name}!`;
+    };
+    console.log('Correct!');
+    return tryGuess(name, numberOfTries - 1);
+};
+
+const guessGameRules = `Answer 'yes' if the number is even, otherwise answer 'no'.`;
+
+export const guessGame = () => { console.log(tryGuess(introduction(guessGameRules), 3)) };
