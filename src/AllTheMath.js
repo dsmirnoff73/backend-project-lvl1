26a54a739fb6b2c8fc626c1e268827a37dfa0f63
Dmitry <d.smirnoff.73@gmail.com>
2019-09-11 16:49:35 +0200

    made isPrime in AllTheMath predicat

--- src/AllTheMath.js
+++ src/AllTheMath.js
@@ -2,9 +2,9 @@ export const getRandom = (min, max) => Math.floor(Math.random() * (max - min) + 
 
 export const isPrime = (num) => {
   for (let divider = 2, controlValue = Math.sqrt(num); divider <= controlValue; divider += 1) {
-    if (num % divider === 0) return 'no';
+    if (num % divider === 0) return false;
   }
-  return num > 1 ? 'yes' : 'no';
+  return num > 1;
 };
 
 export const gcd = (x, y) => ((x === y) ? x : gcd(Math.max(x, y) - Math.min(x, y), Math.min(x, y)));
--- src/games/primeGame.js
+++ src/games/primeGame.js
@@ -6,6 +6,6 @@ export default () => ({
   rules: 'Answer "yes" if given number is prime. Otherwise answer "no".',
   setQuestion: () => getRandom(...rangeForArgumentGeneration),
   questionToString: (question) => String(question),
-  getRightAnswer: (num) => isPrime(num),
+  getRightAnswer: (num) => isPrime(num) ? 'yes' : 'no',
   normalizeAnswer: (answer) => ((answer === 'yes') ? 'yes' : 'no'),
 });
