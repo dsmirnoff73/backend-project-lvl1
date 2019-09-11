40a2d328fabf6f4575a71ee44e5e8c1c1403427f
Dmitry <d.smirnoff.73@gmail.com>
2019-09-11 09:42:44 +0200

    index.js restructured

--- src/index.js
+++ src/index.js
@@ -2,11 +2,25 @@ import readlineSync from 'readline-sync';
 
 export const getRandom = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
-export const introduction = (rulesMssg = '') => {
+const introduction = (rulesMssg = '') => {
     console.log(`\nWelcome to the Brain Games!\n${rulesMssg}`);
     const name = readlineSync.question('\nMay I have your name? : ');
     console.log(`Hello, ${name}!`);
     return name;
+};
+
+const playOneTime = (name, game, numberOfTries) => {
+    if (!numberOfTries) return `\nCongratulations, ${name}!\n`;
+
+    const { setQuestion, questionToString, getRightAnswer, normalizeAnswer } = game;
+    const question = setQuestion();
+    const rightAnswer = getRightAnswer(question);
+    const answer = normalizeAnswer(readlineSync.question(`\nQuestion: ${questionToString(question)}\nYour answer?: `));
+
+    if (rightAnswer !== answer) return `\n'${answer}' is a wrong answer ;(. Correct answer was '${rightAnswer}'.
+        Let's try again, ${name}!\n`;
+    console.log('Correct!');
+    return playOneTime(name, game, numberOfTries - 1);
 };
 
 export const letsPlay = (game, maxNumberOfRounds = 3) => {
@@ -14,18 +28,5 @@ export const letsPlay = (game, maxNumberOfRounds = 3) => {
     const { rules } = game;
     const playersName = introduction(rules);
 
-    const playOneTime = (name, _game, numberOfTries) => {
-        if (!numberOfTries) return `\nCongratulations, ${name}!\n`;
-
-        const { setQuestion, questionToString, getRightAnswer, normalizeAnswer } = game;
-        const question = setQuestion();
-        const rightAnswer = getRightAnswer(question);
-        const answer = normalizeAnswer(readlineSync.question(`\nQuestion: ${questionToString(question)}\nYour answer?: `));
-
-        if (rightAnswer !== answer) return `\n'${answer}' is a wrong answer ;(. Correct answer was '${rightAnswer}'.
-            Let's try again, ${name}!\n`;
-        console.log('Correct!');
-        return playOneTime(name, _game, numberOfTries - 1);
-    };
     return playOneTime(playersName, game, maxNumberOfRounds);
 };
